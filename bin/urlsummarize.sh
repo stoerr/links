a2fc05ea643cd0c83621ff2729e4faba3e0e57ff
Dr. Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2024-02-12 21:37:19 +0000

    automatic gpt-4 if context too large

--- bin/urlsummarize.sh
+++ bin/urlsummarize.sh
@@ -8,7 +8,31 @@ if [ "$#" -ne 1 ]; then
 fi
 
 MODEL="gpt-3.5-turbo-0125"
-# MODEL="gpt-4-0125-preview"
+MODEL4="gpt-4-0125-preview"
 
-curl -s -L "$1" | pandoc --from html --to markdown --markdown-headings=atx --wrap=preserve -s |
-  sed '/^:::/d' | chatgpt - -m $MODEL -p "Content of URL $1:" -sf assets/linktmpl.prompt
+# curl -s -L "$1" | pandoc --from html --to markdown --markdown-headings=atx --wrap=preserve -s |
+#   sed '/^:::/d' | chatgpt - -m $MODEL -p "Content of URL $1:" -sf assets/linktmpl.prompt
+
+MARKDOWN=$(curl -s -L "$1" | pandoc --from html --to markdown --markdown-headings=atx --wrap=preserve -s | sed '/^:::/d')
+
+RES=$(cat <<EOF | chatgpt - -m $MODEL -p "Content of URL $1:" -sf assets/linktmpl.prompt
+$MARKDOWN
+EOF
+)
+
+# if the return status of the last command wasn't 0 try again with MODEL4 because likely a token count limit was reached
+if [ $? -ne 0 ]; then
+  RES=$(cat <<EOF | chatgpt - -m $MODEL4 -p "Content of URL $1:" -sf assets/linktmpl.prompt
+$MARKDOWN
+EOF
+)
+  # if return status still isn't 0, print error message and exit
+  if [ $? -ne 0 ]; then
+    echo "Error: failed to summarize $1"
+    exit 1
+  fi
+fi
+
+cat <<EOF
+$RES
+EOF
