f139181ca058548f720bb12cba64a48e805db3d0
Dr. Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2024-02-12 21:24:55 +0000

    adding some links

--- bin/urlsummarize.sh
+++ bin/urlsummarize.sh
@@ -7,5 +7,8 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
+MODEL="gpt-3.5-turbo-0125"
+# MODEL="gpt-4-0125-preview"
+
 curl -s -L "$1" | pandoc --from html --to markdown --markdown-headings=atx --wrap=preserve -s |
-  sed '/^:::/d' | chatgpt - -p "Content of URL $1:" -sf assets/linktmpl.prompt
+  sed '/^:::/d' | chatgpt - -m $MODEL -p "Content of URL $1:" -sf assets/linktmpl.prompt
