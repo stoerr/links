9f4357921a20ed5545c1b4b053d90d54d390c763
Dr. Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2024-06-07 08:46:52 +0200

    Cron job - background add of links

--- bin/_crontab.sh
+++ bin/_crontab.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 # is added to the crontab to ensure timely publication of links so that the search mechanism works
 cd $(dirname $0)/..
-# exec >> _oncron.log
+exec >> bin/_crontab.sh.log
 date
 bin/_makeAll.sh
+git add -A
+# if there are no changes, don't commit
+if [ -z "$(git status --porcelain)" ]; then
+  echo "No changes to commit"
+  exit 0
+fi
+git commit -m "Cron job - background add of links"
+git push
